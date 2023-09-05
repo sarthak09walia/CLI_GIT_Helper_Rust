2b38fe05e4ed5f901a5b6ec4a7db352eb9a6998e
Sarthak Walia <sarthakwalia09@gmail.com>
2023-09-04 22:33:22 -0700

    added color commit

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,33 @@
+extern crate colored;
 use std::env;
 use std::io;
 use std::process::Command;
-use colored::Colorize;
+use colored::*;
+use crossterm::{
+    cursor,
+    execute,
+    terminal::{self, ClearType},
+};
+
 
 fn main() {
     let initial_path = get_initial_path();
     env::set_current_dir(&initial_path).expect("Failed to change directory");
 
     loop {
-        println!(" ");
-        println!("Git Manager - Please select an option:");
-        println!(" ");
+        clear_screen();
+
+        println!("Git Manager - Please select an option:\n");
         println!("1. Initialize Repo");
         println!("2. Make Commit");
         println!("3. Clone a Repository");
         println!("4. Get Git Status");
-        println!("5. Git pull request");
+        println!("5. Git Pull Request");
         println!("0. Exit");
 
         let mut input = String::new();
-        std::io::stdin().read_line(&mut input).unwrap();
-        let option = input.trim().parse::<i32>().unwrap();
+        io::stdin().read_line(&mut input).unwrap();
+        let option = input.trim().parse::<i32>().unwrap_or(-1);
 
         match option {
             1 => git_init(),
@@ -38,7 +45,10 @@ fn main() {
 }
 
 fn get_initial_path() -> String {
-    println!("Please enter the initial directory path where you want to perform Git operations:");
+    println!(
+        "{}",
+        "Please enter the initial directory path where you want to perform Git operations:".blue().bold()
+    );
     let mut path = String::new();
     io::stdin()
         .read_line(&mut path)
@@ -46,137 +56,71 @@ fn get_initial_path() -> String {
     path.trim().to_string()
 }
 
-fn git_init() {
-    // Execute the command and capture its output
-    let output = Command::new("cmd")
-        .args(["/C", "git init"])
-        .output()
-        .expect("failed to execute process");
+fn clear_screen() {
+    execute!(
+        io::stdout(),
+        terminal::Clear(ClearType::All), // Use ClearType::All to clear the terminal
+        cursor::MoveTo(0, 0)
+    )
+    .expect("Failed to clear terminal");
+}
 
-    // Check if the command was successful
-    if output.status.success() {
-        // Convert the stdout bytes to a string
-        if let Ok(stdout_str) = String::from_utf8(output.stdout) {
-            println!("Command output: {}", stdout_str);
-        } else {
-            println!("Failed to git init");
-        }
-    } else {
-        // Handle command failure
-        let exit_code = output.status.code().unwrap_or(1); // Default to 1 if exit code is None
-        eprintln!("Command failed with exit code: {}", exit_code);
-    }
+fn git_init() {
+    execute_command("git init");
+    pause_for_effect();
 }
 
 fn git_clone() {
-    println!("Please enter github url to clone *only public repositories");
+    println!("Please enter the GitHub URL to clone (public repositories only):");
     let mut url = String::new();
-    std::io::stdin().read_line(&mut url).unwrap();
-
-    let output= Command::new("cmd")
-                .args(["/C","git", "clone", &url])
-                .output()
-                .expect("Failed to clone");
-
-    // Check if the command was successful
-    if output.status.success() {
-        // Convert the stdout bytes to a string
-        if let Ok(stdout_str) = String::from_utf8(output.stdout) {
-            println!("Clone Success {}", stdout_str);
-        } else {
-            println!("Failed to capture command output");
-        }
-    } else {
-        // Handle command failure
-        let exit_code = output.status.code().unwrap_or(1); // Default to 1 if exit code is None
-        eprintln!("Command failed with exit code: {}", exit_code);
-    }
+    io::stdin().read_line(&mut url).unwrap();
+    execute_command(&format!("git clone {}", url.trim()));
+    pause_for_effect();
 }
 
 fn git_commit() {
-    // Execute the command and capture its output, adding files before committing
-    let output = Command::new("cmd")
-        .args(["/C", "git", "add", "."])
-        .output()
-        .expect("failed to execute process");
-
-    // Check if the command was successful
-    if output.status.success() {
-        println!("Added files to git");
-    } else {
-        // Handle command failure
-        let exit_code = output.status.code().unwrap_or(1); // Default to 1 if exit code is None
-        eprintln!("Command failed with exit code: {}", exit_code);
-    }
-
-    //getting git commit message
+    execute_command("git add .");
     println!("Please enter commit message:");
     let mut message = String::new();
-    std::io::stdin().read_line(&mut message).unwrap();
-
-    // Remove leading and trailing whitespace from the message
+    io::stdin().read_line(&mut message).unwrap();
     let message = message.trim();
-
-    // Execute the command and capture its output
-    let output = Command::new("cmd")
-        .args(["/C", "git", "commit", "-m", message])
-        .output()
-        .expect("failed to execute process");
-
-    // Check if the command was successful
-    if output.status.success() {
-        // Convert the stdout bytes to a string
-        if let Ok(stdout_str) = String::from_utf8(output.stdout) {
-            println!("Command output: {}", stdout_str);
-        } else {
-            println!("Failed to capture command output");
-        }
-    } else {
-        // Handle command failure
-        let exit_code = output.status.code().unwrap_or(1); // Default to 1 if exit code is None
-        eprintln!("Command failed with exit code: {}", exit_code);
-    }
+    execute_command(&format!("git commit -m '{}'", message));
+    pause_for_effect();
 }
 
 fn git_status() {
-    let output = Command::new("cmd")
-        .args(["/C","git", "status"])
-        .output()
-        .expect("failed to get status");
+    execute_command("git status");
+    pause_for_effect();
+}
 
+fn git_pull() {
+    println!("Enter branch name:");
+    let mut branch_name = String::new();
+    io::stdin().read_line(&mut branch_name).unwrap();
+    let branch_name = branch_name.trim();
+    execute_command(&format!("git pull origin {}", branch_name));
+    pause_for_effect();
+}
+
+fn execute_command(command: &str) {
+    let output = Command::new("cmd")
+        .args(["/C", command])
+        .output()
+        .expect("Failed to execute process");
 
     if output.status.success() {
-        // Convert the stdout bytes to a string
         if let Ok(stdout_str) = String::from_utf8(output.stdout) {
-            println!("{}", stdout_str);
+            println!("Command output: {}", stdout_str.green());
         } else {
             println!("Failed to capture command output");
         }
     } else {
-        // Handle command failure
-        let exit_code = output.status.code().unwrap_or(1); // Default to 1 if exit code is None
+        let exit_code = output.status.code().unwrap_or(1);
         eprintln!("Command failed with exit code: {}", exit_code);
     }
 }
 
-fn git_pull(){
- println!("Enter branch name: ");
-
- let mut branch_name = String::new();
- io::stdin().read_line(&mut branch_name).unwrap();
-
- let output = Command::new("cmd").args(["/C","git","pull","origin", &branch_name]).output().expect("git pull FAILED");
-
- if output.status.success() {
-    // Convert the stdout bytes to a string
-    if let Ok(stdout_str) = String::from_utf8(output.stdout) {
-        println!("{}", stdout_str);
-    } else {
-        println!("Failed to capture command output");
-    }
-} else {
-    // Handle command failure
-    let exit_code = output.status.code().unwrap_or(1); // Default to 1 if exit code is None
-    eprintln!("Command failed with exit code: {}", exit_code);
+fn pause_for_effect() {
+    std::thread::sleep(std::time::Duration::from_secs(2));
 }
-}
+
